ac9a6e9d985b0158926b045b0940154f4f744852
Aditya Chine <adityahemantchine41@gmail.com>
2025-04-15 15:58:43 +0530

    Update python_leapyear_cheacker.py

--- Python_if_statement/python_leapyear_cheacker.py
+++ Python_if_statement/python_leapyear_cheacker.py
@@ -1,11 +1,9 @@
-yourname= input("Your Name Please...!:->")
-print("Hii", yourname)
-l_year =int(input("Please Enter Year that you want to check:->"))
+yourname = input("Your Name Please...!:-> ")
+print("Hi", yourname)
 
-divforyear= (l_year%4)
+l_year = int(input("Please Enter Year that you want to check:-> "))
 
-if divforyear == 0:
-    print("yes it's a leap year",yourname,"Happy Leap year......!")
+if (l_year % 4 == 0 and l_year % 100 != 0) or (l_year % 400 == 0):
+    print("Yes,", yourname + ", it's a leap year! 🎉 Happy Leap Year!")
 else:
-    print("Sorry ",yourname,"it's not a leap year ")
-
+    print("Sorry", yourname + ", it's not a leap year.")
